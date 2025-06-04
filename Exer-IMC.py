63bf16a267aefaaaea667514726a5b119f4f56d1
AndreFTeixeira7 <andrefelipeteixeira7@gmail.com>
2025-06-04 17:35:27 -0400

    Adiciona os arquivos das aulas 10 a 14 e exercicio de imc

---
+++ Exer-IMC.py
@@ -0,0 +1,11 @@
+# Calcular o imc
+
+nome = "André Felipe"
+idade = 25
+altura = 1.81
+peso = 64
+imc = peso/ (altura * altura)
+
+# IMC = PESO / ALTURA²
+
+print(f"{nome} tem altura de  {altura}, pesa {peso} Quilos e seu imc é {imc:.2f}")
